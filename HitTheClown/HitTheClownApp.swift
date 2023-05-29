ed3279d388c69e924da9ba69829441260a6a6df1
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-29 03:18:20 -0400

    Initial Commit

---
+++ HitTheClown/HitTheClownApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitTheClownApp.swift
+//  HitTheClown
+//
+//  Created by Lori Rothermel on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct HitTheClownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
